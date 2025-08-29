9e8496fcaa8b6e79564ecb64250551665279d582
LizDautt16 <lizrespaldo@gmail.com>
2025-08-29 15:54:25 -0700

    Consultar_Registrar_Editar_Eliminar_Guardar

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -1,26 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
-
+#use App\Models\Alumno;
 use Illuminate\Http\Request;
 
 class AlumnoController extends Controller
 {
     //Santillan
     public function consulta(){
-        $nombre = "slkmfjlksdmflksdmlkf";
-        $edad = 99;
+        $nombre = "Luis Carlo Caro Dautt";
+        $edad = 7;
         $numero_control = "0234117823634";
         
         return view("ConsultaAlumnos", compact('nombre', 'edad', 'numero_control'));
 }
 
  public function calcular(){
-        $calificaciones[0]= 8;
-        $calificaciones[1]= 10;
-        $calificaciones[2]= 7;
-        $calificaciones[3]= 5.5;
-        $calificaciones[4]= 6.8;
+        $calificaciones[0]= 7;
+        $calificaciones[1]= 9;
+        $calificaciones[2]= 8;
+        $calificaciones[3]= 6.5;
+        $calificaciones[4]= 7.3;
 
         $nombre = "Luis";
 
@@ -30,7 +30,7 @@ class AlumnoController extends Controller
     public function getAlumnos(){
         $alumnos = Alumno::orderBy('id', 'desc')->get();
         return view("ConsultarAlumnos", compact('alumnos'));
-    }
+    }// el compact en algunos dice alumno y entros alumnos
 
     public function registrarAlumno(){
         return view('RegistrarAlumno');
@@ -61,7 +61,7 @@ class AlumnoController extends Controller
 
     public function editarAlumno($id){
         $alumno = Alumno::find($id);
-
+            //no es compact 'alumnos' ??
         return view('EditarAlumno', compact('alumno'));
     }
 
